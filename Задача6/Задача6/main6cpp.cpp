a3c3a85e610dec785a9344d67f807f005cd4ea0c
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-20 22:26:17 +0300

    готовая 6 задача

--- Задача6/Задача6/main6cpp.cpp
+++ Задача6/Задача6/main6cpp.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 void input_array(int dig[][5]) {
-	int number = 0;
+	int number = 1;
 	int mult = 1;
 	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {                                   // 0 1 2 3 4 4 3 2 1 0 4
-			dig[i][(number%10 - i % 2 * (j * 2 + 1))] = number++;                      // 4 3 2 1 0
+		for (int j = 0; j < 5; ++j) {                   
+			dig[i][(5 + j * mult - i % 2) % 5] = number++;
 		}
-		cout << i % 2;
 		mult *= (-1);
 	}
 }
